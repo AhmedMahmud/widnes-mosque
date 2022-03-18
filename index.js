bbd071027c21240916f562664300beb1e03fcc52
Ahmed Mahmud <ahmed@LAPTOP-7UNV4AFC.localdomain>
2022-03-18 11:15:11 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.use(express.static('public'))
+app.listen(process.env.port || 3000);
+
+console.log('Running');
